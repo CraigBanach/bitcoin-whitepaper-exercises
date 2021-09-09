bb222f455e731a1a4aabaae88a4d9bbbcdbf648b
Craig Banach <craigsb93@gmail.com>
2021-09-09 23:19:22 +0100

    Complete hashing exercise

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -1,10 +1,46 @@
 "use strict";
 
-var crypto = require("crypto");
+const crypto = require("crypto");
+
+function createBlock(data) {
+	const block = {
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+		data,
+		timestamp: Date.now(),
+	};
+	block.hash = blockHash(block);
+
+	return block;
+};
+
+function blockHash(block) {
+	return crypto.createHash("sha256")
+		.update(`${block.index};${block.prevHash};${block.data};${block.timestamp};`)
+		.digest("hex");
+} 
+
+function verifyChain(chain) {
+	if (chain.blocks.length === 0) return true;
+	const block = chain.blocks.pop();
+	const prevHash = chain.blocks.length > 0 ? chain.blocks[chain.blocks.length - 1].hash : null;
+	return !!(verifyChain(chain) && verifyBlock(block, prevHash));
+}
+
+function verifyBlock(block, prevBlockHash) {
+	const hash = block.hash;
+	delete block.hash;
+	const computedHash = blockHash(block);
+
+	return (block.data && block.data.length !== 0)
+		&& (block.index === 0 || block.prevHash && block.prevHash.length !== 0 && block.prevHash === prevBlockHash)
+		&& (block.index >= 0)
+		&& (hash === computedHash || (block.index === 0 && hash === "000000"));
+}
 
 // The Power of a Smile
 // by Tupac Shakur
-var poem = [
+const poem = [
 	"The power of a gun can kill",
 	"and the power of fire can burn",
 	"the power of wind can chill",
@@ -15,7 +51,7 @@ var poem = [
 	"especially yours can heal a frozen heart",
 ];
 
-var Blockchain = {
+const Blockchain = {
 	blocks: [],
 };
 
@@ -23,21 +59,23 @@ var Blockchain = {
 Blockchain.blocks.push({
 	index: 0,
 	hash: "000000",
-	data: "",
+	data: "Genesis",
 	timestamp: Date.now(),
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	Blockchain.blocks.push(createBlock(line));
+};
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+// For testing an invalid blockchain:
 
+// Blockchain.blocks.push({
+// 	index: 9,
+// 	prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+// 	hash: "1",
+// 	data: "Fake block",
+// 	timestamp: Date.now(),
+// });
 
-// **********************************
-
-function blockHash(bl) {
-	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
-	).digest("hex");
-}
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
